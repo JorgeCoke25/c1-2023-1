ca46a1fff3672323b073106fd91ef69a7a68ea0f
Jorge_Barrios <jorge.barriosn@usm.cl>
2023-04-20 14:24:22 -0400

    Expliacion de rutas agregado

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,11 @@ import users from './user/user'
 const router = new Router()
 
 router.get('/health', getHealth)
-
+//RUta que optiene todos los usuarios
 router.get('/api/users',users.getUsers);
-
+//Obtiene todos los usarios por un nombre
 router.get('/api/users/:name', users.getUsersActiveByName);
-
+//Obtiene todos los ususarios por parametros pasados por un POST
 router.post('/api/user',users.getUsersByParameters);
 
 export default router
